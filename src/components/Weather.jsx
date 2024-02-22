a74703a9e7e0b6ed7278dcd2a8aae56cd448f3ea
systemhunter <systemhunter@localhost.localdomain>
2024-02-22 19:05:10 +0400

    Adding loader effect.

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,11 @@
-import React,{ useState } from "react";
+import React,{ useState, useEffect } from "react";
 
 export const Weather = () => {
 
     const apiKey="49fa40898a6aebcad9c875eeb9f6b5d3";
     const [weatherData, setWeatherData] = useState([]);
     const [city, setCity] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const getWeather = (event) => {
         if(event.key == 'Enter') {
@@ -21,10 +22,22 @@ export const Weather = () => {
         }
     }
 
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 2500)
+    }, [])
+
 
     return(
         <div className="parent-container">
-            <div className="container">
+            {loading ? (
+                <div className="loader-container">
+                    <div className="loader-spinner"><img src="/images/icons/loader.png" /></div>
+                </div>
+            ) : (
+                <div className="container">
                 <div className="weather-header">
                     <h1>Welcome to weather app</h1>
                     <input 
@@ -38,13 +51,13 @@ export const Weather = () => {
                 {typeof weatherData.main !== 'undefined' &&
                     <div className="weather-body">
                         <ul>
-                            <li><span>Location:</span> {weatherData.name}</li>
+                            <li><span>Location:</span> {weatherData.name}, {weatherData.sys.country}</li>
                             <li><span>Temperature:</span> {weatherData.main.temp}°F</li>
                             <li><span>Feels Like:</span> {weatherData.main.feels_like}°F</li>
                             <li><span>Weather:</span> {weatherData.weather[0].main}, {weatherData.weather[0].description}</li>
                             <li><span>Humidity:</span> {weatherData.main.humidity}%</li>
                             <li><span>Wind Speed:</span> {weatherData.wind.speed} mph</li>
-                        </ul>
+                           </ul>
                     </div> 
                 }
                 {weatherData.cod === '404' &&
@@ -53,7 +66,8 @@ export const Weather = () => {
                     </div> 
                 }
                 </div>
-            </div>
+            )}
+        </div>
     )
 }
 
